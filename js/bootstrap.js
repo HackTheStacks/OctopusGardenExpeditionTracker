84965023b8821ab61762baf56d263d3a2ccedbcd
Jeff Jassky <jeff@jeffjassky.com>
2016-11-19 21:50:01 -0500

    Modified some files

---
+++ js/bootstrap.js
@@ -0,0 +1,22 @@
+$.fn.serializeObject = function(){
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
+
+jQuery(function(){
+	$('form').on('submit', function(event){
+		event.preventDefault();
+		var data = $(this).serializeObject();
+	});
+});
